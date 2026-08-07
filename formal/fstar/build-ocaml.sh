ed67f85920e0ccd9f93c9e3839955768b3191d2f
Dan Brickley <danbri@danbri.org>
2026-08-07 12:04:06 +0000

    build: add RhoDFClosure.ml to the npm-entry link array (hazard 11 -- the wiring added the extraction entry but not the link entry)

--- formal/fstar/build-ocaml.sh
+++ formal/fstar/build-ocaml.sh
@@ -2211,7 +2211,7 @@ if [[ "$STEP" == "all" || "$STEP" == "js" ]]; then
     RDF_GraphIsomorphism.ml
     GRDDL_Discovery.ml
     service_wrap_hook.ml
-    SPARQL_FullText.ml SPARQL11_Algebra.ml XSD_Datatypes.ml XSD_IEEE754.ml RDF_Entailment_Regime.ml XForms_Bind.ml RDF_Pretty.ml OWL_QueryRewrite.ml OWL_QueryEval.ml OWL_Tests_Manifest.ml OWL2_SyntaxDL.ml RIF_Core_Syntax.ml Parser_RIFXML.ml RIF_Core_Translation.ml RIF_Core_Builtins.ml RIF_Core_Conformance.ml RIF_Core_Eval.ml RIF_Core_Tests.ml SPARQL11_Parser.ml SHACL_Validation.ml SHACL_NodeExpr.ml SHACL_Rules.ml
+    SPARQL_FullText.ml SPARQL11_Algebra.ml XSD_Datatypes.ml XSD_IEEE754.ml RDF_Entailment_Regime.ml RDF_Entailment_RDFS_RhoDFClosure.ml XForms_Bind.ml RDF_Pretty.ml OWL_QueryRewrite.ml OWL_QueryEval.ml OWL_Tests_Manifest.ml OWL2_SyntaxDL.ml RIF_Core_Syntax.ml Parser_RIFXML.ml RIF_Core_Translation.ml RIF_Core_Builtins.ml RIF_Core_Conformance.ml RIF_Core_Eval.ml RIF_Core_Tests.ml SPARQL11_Parser.ml SHACL_Validation.ml SHACL_NodeExpr.ml SHACL_Rules.ml
     ShEx_Schema.ml Parser_ShExC.ml ShEx_SchemaEq.ml ShEx_Validation.ml
     RML_Mapping.ml RML_Sources.ml RML_Eval.ml
     CSVW_Metadata.ml CSVW_URITemplate.ml CSVW_Formats.ml CSVW_Conversion.ml CSVW_Json.ml CSVW_Validate.ml
